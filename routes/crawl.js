c9ea1b05202b3ac16351f0aa04c2177ea849f3fe
duong <trananh123duong@gmail.com>
2025-10-01 19:05:02 +0700

    edit crawl one chapter

--- routes/crawl.js
+++ routes/crawl.js
@@ -1,6 +1,6 @@
 // routes/crawl.js
 const express = require('express');
-const { crawlBySlug, crawlPages } = require('../controllers/crawl.controller');
+const { crawlBySlug, crawlPages, crawlOneChapter } = require('../controllers/crawl.controller');
 const router = express.Router();
 
 router.get('/', crawlPages);          // ?pages=2
